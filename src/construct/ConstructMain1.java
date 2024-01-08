24fb6e1cbba5394aa355d98ef6fe224a51a8ef22
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2024-01-08 14:42:44 +0900

    생성자 코드 작성

---
+++ src/construct/ConstructMain1.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class ConstructMain1 {
+    public static void main(String[] args) {
+
+
+    MemberConstruct member1 = new MemberConstruct("user1",15,80);
+    MemberConstruct member2 = new MemberConstruct("user2" , 14, 80);
+
+        MemberConstruct[] members = {member1, member2};
+
+        for (MemberConstruct  s : members) {
+        System.out.println(s.name+s.age+s.grade);
+    }
+}
+}
