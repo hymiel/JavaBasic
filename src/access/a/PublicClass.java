c24a9286fc2f398b15bb218f5c2ef1e3f336c902
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2024-01-10 17:50:08 +0900

    패키지 레벨의 접근제어자

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,19 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+
+        PublicClass publicClass = new PublicClass();
+        DafaultClass1 Class1 = new DafaultClass1();
+        DefaultClass2 Class2 = new DefaultClass2();
+    }
+
+
+}
+class DafaultClass1 {
+
+}
+
+class DefaultClass2 {
+
+}
