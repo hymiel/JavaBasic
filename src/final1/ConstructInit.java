f2a48169ec9ab5426780e600cc35add4bdc1c73e
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2024-01-16 14:38:49 +0900

    필드와 생성자 초기화

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+    final int valus;
+
+    //할당이 안되서 생성자로 초기화
+    public ConstructInit(int valus) {
+        this.valus = valus;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    //이미 할당이 된 경우 생성자에서 초기값불가
+
+}
