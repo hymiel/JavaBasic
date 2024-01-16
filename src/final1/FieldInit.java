e604dddf31ac1425b00df8b71cbf247d9c8925a9
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2024-01-16 15:01:31 +0900

    상수 예시

--- src/final1/FieldInit.java
+++ src/final1/FieldInit.java
@@ -1,7 +1,7 @@
 package final1;
 
 public class FieldInit {
-    static final int CONST_VALUE = 10;
+    static final int CONST_VALUE = 10; //공용변수인데 바뀌지 않는 변수
     final int value = 10;
 
     //이미 할당이 된 경우 생성자에서 초기값불가
