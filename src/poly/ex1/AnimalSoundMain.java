3bf094f1ab57a8d377d27f287f55406594564e53
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2024-01-18 16:21:03 +0900

    타입이 다 다르기 때문에 배열이나 for문으로 중복제거가 불가능

--- src/poly/ex1/AnimalSoundMain.java
+++ src/poly/ex1/AnimalSoundMain.java
@@ -5,7 +5,7 @@ public class AnimalSoundMain {
         //타입이 다 다르기 때문에 배열이나 for문으로 중복제거가 불가능하다.
         Dog dog = new Dog();
         Cat cat = new Cat();
-        Caw caw = new Caw();
+        Cow cow = new Cow();
 
         System.out.println("동물 소리 테스트 시작");
         dog.sound();
@@ -15,12 +15,12 @@ public class AnimalSoundMain {
         cat.sound();
         System.out.println("동물 소리 테스트 종료");
 
-        sound(caw);
+        sound(cow);
     }
-    private static void sound(Caw caw) {
+    private static void sound(Cow cow) {
 
         System.out.println("동물 소리 테스트 시작");
-        caw.sound();
+        cow.sound();
         System.out.println("동물 소리 테스트 종료");
     }
 }
